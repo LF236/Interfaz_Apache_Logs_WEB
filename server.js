19adbeb2613c0db92661ea170c5738b05a0b802d
LF236 <fernandorodriguez236@gmail.com>
2021-03-17 20:54:02 -0600

    Se terminan los metodos para la lectura de la data y la creación del JSON

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
+//-----------Procesamiento de datos------------------
 const fs = require('fs');
 const nameFile = 'access_log';
 
-//Esta función es para leer el contenido del archivo
+//Esta función es para leer el contenido del archivo es una promesa para el manejo de callbacks
 const generateDocumentJSON = (routeFile) => {
     return new Promise((resolve,reject) => {
-        let json = "[";
+        let json = '[';
         fs.readFile(routeFile,'utf-8',(error,data) => {
             //Si el archivo no existe
             if(error) {
@@ -13,6 +14,9 @@ const generateDocumentJSON = (routeFile) => {
             }
             //Empezamos a filtrar la data
             const aux = data.split('\n');
+            //Auxiliar para no imprimir una coma al último siempre
+            const auxLength = aux.length;
+            let count = 0;
             //console.log(aux.length);
             aux.forEach(line => {
                 line = line.replace('"','');
@@ -23,7 +27,7 @@ const generateDocumentJSON = (routeFile) => {
                 let dateComplete = auxSplit[3];
                 let hour;
                 try {
-                    dateComplete = dateComplete.replace("[","");
+                    dateComplete = dateComplete.replace('[','');
                     dateComplete = dateComplete.split(':');
                     hour = `${dateComplete[1]}:${dateComplete[2]}:${dateComplete[3]}`;
                     dateComplete = dateComplete[0];
@@ -37,38 +41,56 @@ const generateDocumentJSON = (routeFile) => {
                 //Creamos el fragmeto con línea
                 const jsonElement = `
                     {
-                        ip: ${dirIp},
-                        date: ${dateComplete},
-                        hour: ${hour},
-                        method: ${method},
-                        resource: ${resource},
-                        protocol: ${protocol},
-                        codeRequest: ${codeRequest}
-                    },
+                        "ip": "${dirIp}",
+                        "date": "${dateComplete}",
+                        "hour": "${hour}",
+                        "method": "${method}",
+                        "resource": "${resource}",
+                        "protocol": "${protocol}",
+                        "codeRequest": "${codeRequest}"
+                    }
                 `;
+                count += 1;
                 json += jsonElement;
+                if(count < auxLength) json += ',';
             })
-            json += "]";
+            json += ']';
             resolve(json);
         })
     })
 }
 
-const getData = async () => {
+const writeJSON = (data) => {
+   fs.writeFileSync('logs.json',data,'UTF-8', (err) => {
+       if(err) {
+           console.log('TODO SALIO MAL');
+       }
+       else {
+           console.log('Its OK');
+           fs.readFileSync('logs.json',"utf-8");
+       }
+   })
+}
+
+
+const loadJSON = async () => {
     try {
         const data = await generateDocumentJSON(nameFile);
-        return data;
+        await writeJSON(data);
+        return true;
     }
     catch (error){
-        console.log('EL ARCHIVO NO SE HA PROCESADO CORRECTAMENTE');
+        console.log('Todo Salio Mal');
         throw error;
     }
 }
 
-getData()
+loadJSON()
     .then(data => {
         console.log(data);
     })
     .catch(err => {
         console.log(err);
     })
+
+//----------------Servidor-----------
